a8f1bf8a375fa0c6ddf909373b446da19048794d
ouandji02 <ouandjithierry00@gmail.com>
2021-12-22 13:05:09 +0100

    funcition.h

---
+++ function.h
@@ -0,0 +1,16 @@
+int triFusion(int *tab, int debut, int fin){
+    int milieu = (debut+fin)/2;
+    if(n==1 || n==0){
+        return tab;
+    }
+    
+    fusionner(tab, debut, milieu);
+    fusionner(tab,  milieu+1, fin);
+}
+
+int fusionner(int *tab, int debut, int fin){
+    int c[fin-debut+1];
+    int milieu = (debut+fin)/2
+    int k,i,j;
+    for(i=0;i<)
+}
